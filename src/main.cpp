bb6230a3ff749dddabe4b2c1d3c47202b977ff15
andre gomes <antoniogomes8>
2020-12-19 22:28:16 +0000

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(13, OUTPUT);
+  
+}
+
+void loop() {
+  int sensor = digitalRead(4);
+
+  Serial.println(sensor);
+  if (sensor == HIGH)
+  {
+    digitalWrite(13, LOW);
+  } else
+  {
+    digitalWrite(13, HIGH);
+  }
+  
+  
+  
+}
